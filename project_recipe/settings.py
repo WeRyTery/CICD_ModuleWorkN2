fe0e8fffc8adaacefa676ebeeda0cae352aea66f
Ghost <133059737+WeRyTery@users.noreply.github.com>
2026-05-21 13:24:58 +0300

    Add: Heroku setup
    Added heroku preperation by adding ci.yml workflow, pytest.ini.
    Updated requirements.txt and settings

--- project_recipe/settings.py
+++ project_recipe/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9jo7t)*7&hff$4x9j%37qy(o+&ywxxm@7*@b##!4#f%kxg(_-l'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
